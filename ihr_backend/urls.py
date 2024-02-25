0530b7a12196503ea65c6e15fb9ca87c7c0cb048
HalloweenJack2606 <mcoloniab18@gmail.com>
2024-02-25 16:19:34 -0500

    api url change

--- ihr_backend/urls.py
+++ ihr_backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('ihr_api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
